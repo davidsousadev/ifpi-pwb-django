ec23e613e65e9ca7167205a673b31133ce60ef2f
davidsousadev <davidk1k3k@gmail.com>
2025-01-22 19:07:43 -0300

    Ajuste projeto do professor

--- gestaodeprojeto/gestaodeprojeto/urls.py
+++ gestaodeprojeto/gestaodeprojeto/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-admin.site.site_header = 'Gestão de Projeto'
-admin.site.index_title = 'Cadastros e Consultas'
+admin.site.site_header = 'R1 Project Management'
+admin.site.index_title = 'Operações'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
